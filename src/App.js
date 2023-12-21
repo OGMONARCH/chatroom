5010434688eda5bacf697ac9ec680e0f13c7f461
OGMONARCH <149230226+OGMONARCH@users.noreply.github.com>
2023-12-21 09:41:16 +0100

    kjefL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { ChatEngine } from 'react-chat-engine'
-import { ChatFeed } from './component/ChatFeed'
+import ChatFeed from './components/ChatFeed';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
    <ChatEngine 
     height="100vh"
@@ -12,8 +15,12 @@ const App = () => {
     userName="King"
     //userSecret is simply the password
     userSecret="12341234"
+    // userName={localStorage.getItem('username')}
+    // userSecret={localStorage.getItem('password')}
     renderChatFeed={(chatAppProps)=><ChatFeed {... chatAppProps}/>}
+    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
    />
+// infinite scroll, logout, more customizations...
   )
 }
 
